a5ff4618b6fc175f74bb82a1e418adb658ef6ef4
Yoshiyukibabaip12 <178962038+yoshiyukibabaip12@users.noreply.github.com>
2025-01-14 21:34:37 +0900

    Vite manifestの調整

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js', 'resources/sass/app.scss'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+    rollupOptions: {
+      input: {
+        app: '/resources/js/app.js',
+        style: '/resources/sass/app.scss',
+      },
+    },
+  },
 });
+
